044c607d5eb9497c0ffb6cf3eebe84f3e7a05ea0
marius-dev <iliescu.marius.a@gmail.com>
2017-06-05 18:45:31 +0000

    Create rest controller for calendar creation and render a simple view

---
+++ src/main/java/com/itt/controller/CalendarController.java
@@ -0,0 +1,69 @@
+package com.itt.controller;
+
+import com.itt.service.CalendarService;
+import com.mongodb.BasicDBObject;
+import com.mongodb.util.JSON;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.HttpRetryException;
+import java.text.ParseException;
+
+@RestController
+public class CalendarController {
+
+    private final CalendarService calendarService;
+
+
+    @Autowired
+    public CalendarController(CalendarService calendarService) {
+        this.calendarService = calendarService;
+    }
+
+    @RequestMapping(value = "/save-calendar", method= RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity<String> saveCalendarAction(@RequestBody String newStructure) {
+        try {
+            this.calendarService.saveAcademicYearFormCalendar(newStructure);
+        } catch (ParseException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>("saved",HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/get-week-by-date", method= RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity getWeekByDateAction(@RequestBody String dataAsJson) {
+        try {
+
+            BasicDBObject data = (BasicDBObject) JSON.parse(dataAsJson);
+
+            Integer result = this.calendarService.getWeekNumber(
+                    (String)  data.get("date"),
+                    (String)  data.get("academic_year"),
+                    (Integer) data.get("semester"),
+                    (Integer) data.get("year_of_study"),
+                    (String)  data.get("specialization")
+            );
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+
+        } catch (ParseException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (HttpRetryException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.valueOf(e.responseCode()));
+        }
+    }
+
+
+        @RequestMapping("/get_file")
+    public ResponseEntity<String> getFileContent() {
+          return ResponseEntity.status(HttpStatus.CONFLICT).body("eee");
+    }
+}
