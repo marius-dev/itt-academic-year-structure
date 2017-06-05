e2a424712aedc4f2fabf1101f6faf6840804def2
marius-dev <iliescu.marius.a@gmail.com>
2017-06-05 18:40:02 +0000

    Integrate spring boot with mongo drivers

---
+++ src/main/java/com/itt/IttSuApplication.java
@@ -0,0 +1,12 @@
+package com.itt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IttSuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IttSuApplication.class, args);
+	}
+}
